ddbed6d4a1501912c52caa050a06f9ad696e47b2
sealandsigh <zhujiajunzhu@qq.com>
2019-07-16 23:32:07 +0800

    实现调用ansible接口获取资产写库

--- asset/management/commands/collect_host.py
+++ asset/management/commands/collect_host.py
@@ -5,6 +5,7 @@
 import shutil
 import ansible.constants as C
 import os
+import json
 
 from django.core.management import BaseCommand
 from django.conf import settings
@@ -17,11 +18,32 @@ from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
 from ansible.plugins.callback import CallbackBase
 
+from asset.models import Hosts
 
 class ResultCallback(CallbackBase):
     def v2_runner_on_ok(self, result, **kwargs):
+        self.actions = {
+            'collect_host': self.collect_host
+        }
+        func = self.actions.get(result.task_name)
+        if func:
+            func(result._result)
         print(result._host)
         print(result.task_name)
+
+    def collect_host(self,result):
+        # print(result)
+        fact = result.get('ansible_facts',{})
+        ip = fact.get("ansible_default_ipv4").get("network",'')
+        name = fact.get("ansible_nodename",'')
+        mac = fact.get("ansible_default_ipv4").get("macaddress",'')
+        os = fact.get("ansible_os_family",'')
+        arch = fact.get("ansible_architecture",'')
+        mem = fact.get("ansible_memtotal_mb",'')
+        cpu = fact.get("ansible_processor_cores",'')
+        disk = [{'name':i.get("device"),'total':i.get("size_total")} for i in fact.get("ansible_mounts",[])]
+        Hosts.create_or_replace(name,ip,mac,os,arch,mem,cpu,json.dumps(disk))
+
 
 class Command(BaseCommand):
 
@@ -30,7 +52,7 @@ class Command(BaseCommand):
         Options = namedtuple('Options',
                              ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check',
                               'diff'])
-        options = Options(connection='local', module_path=[], forks=10, become=None, become_method=None,
+        options = Options(connection='smart', module_path=[], forks=10, become=None, become_method=None,
                           become_user=None, check=False, diff=False)
 
         loader = DataLoader()  # Takes care of finding and reading yaml, json and ini files
---
+++ etc/test1.py
@@ -0,0 +1,3 @@
+# l1 = list(range(1,11))
+# print([{'name':n**2} for n in l1 if n % 2 == 0])
+# print({ n+2 : n**2 for n in l1 if n % 2 == 0})
