9a3ceb231b6c7885cbedd3003ccdc0af47dde1ec
sealandsigh <zhujiajunzhu@qq.com>
2019-07-19 12:05:27 +0800

    资产删除功能完成

--- asset/urls.py
+++ asset/urls.py
@@ -10,4 +10,5 @@ app_name='asset'
 urlpatterns = [
     path('index/',views.index,name='index'),
     path('list/ajax/',views.list_ajax,name='list_ajax'),
+    path('delete/ajax/',views.delete_ajax,name='delete_ajax'),
 ]
--- asset/views.py
+++ asset/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from asset.models import Hosts
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -10,6 +11,18 @@ def index(request):
 def list_ajax(request):
     if request.session.get('user') is None:
         redirect('user:login')
-        return JsonResponse({'code':403})
+        return JsonResponse({'code':403,'result':[]})
     result = [ host.as_dict() for host in Hosts.objects.all() ]
     return JsonResponse({'code':200,'result':result})
+
+
+def delete_ajax(request):
+    if request.session.get('user') is None:
+        redirect('user:login')
+        return JsonResponse({'code': 403})
+    uid = request.GET.get('uid')
+    try:
+        Hosts.objects.get(pk=uid).delete()
+        return JsonResponse({'code': 200})
+    except  ObjectDoesNotExist as e:
+        return JsonResponse({'code':400,'errors':e})
