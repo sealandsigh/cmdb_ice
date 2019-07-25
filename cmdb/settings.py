c24ac8ca2b4222bb1bd3fd7b87fabe6d3c5bd521
sealandsigh <zhujiajunzhu@qq.com>
2019-07-25 14:15:26 +0800

    创建web日志分析app项目完成

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
     'asset.apps.AssetConfig',
+    'webanalysis.apps.WebanalysisConfig',
 ]
 
 MIDDLEWARE = [
---
+++ webanalysis/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2019/7/25
+
+from django.urls import path
+from webanalysis import views
+
+app_name='webanalysis'
+
+urlpatterns = [
+    path('index/',views.index,name='index'),
+]
