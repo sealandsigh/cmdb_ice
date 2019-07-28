221afd24d1ba3fe2dd33503951430d08eaaba7dc
sealandsigh <zhujiajunzhu@qq.com>
2019-07-28 12:04:50 +0800

    上传日志分析，分析command完成，数据库model编写完毕，就差分析日志功能

--- webanalysis/management/commands/accessfiletodb.py
+++ webanalysis/management/commands/accessfiletodb.py
@@ -5,8 +5,11 @@
 import os
 import json
 import time
+import traceback
+from datetime import datetime
 from django.conf import settings
 from django.core.management import BaseCommand
+from webanalysis.models import AccessLog
 
 class Command(BaseCommand):
     def handle(self,*args,**kwargs):
@@ -19,10 +22,34 @@ class Command(BaseCommand):
                     path_notice = os.path.join(path,filename)
                     with open(path_notice,'rt') as handler:
                         notice = json.loads(handler.read())
-
-                    self.parse(notice)
+                    try:
+                        self.parse(notice)
+                    except BaseException as e:
+                        print(e)
+                        print(traceback.format_exc())
                     os.unlink(path_notice)
             time.sleep(5)
 
     def parse(self,notice):
         print(notice)
+        path = notice["path"]
+        file_id = notice["id"]
+        with open(path, 'rt') as fhandler:
+            for line in fhandler:
+                try:
+                    line = line.split()
+                    ip = line[0]
+                    print(ip)
+                    url = line[6]
+                    status = line[8]
+                    logtime = line[3]
+                    logtime = logtime[1:]
+                    logtime = datetime.strptime(logtime, '%d/%b/%Y:%H:%M:%S')
+                    logtime = logtime.strftime('%Y-%m-%d %H:%M:%S')
+                    print(logtime)
+                    obj = AccessLog(file_id=file_id,ip=ip,url=url,status_code=status,access_time=logtime)
+                    obj.save()
+                except BaseException as e:
+                    print(e)
+                    print(line)
+        print('parse over: {0}'.format(path))
